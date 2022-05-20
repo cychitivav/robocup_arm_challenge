c7bcd27314fa54ecb469dce536781924c489e96d
jsduenass <jsduenass@unal.edu.co>
2022-05-20 12:32:03 -0500

    refactor: subscribers sensor names

--- src/main.m
+++ src/main.m
@@ -1,7 +1,10 @@
 clear, clc, close all
 %% Connect to ROS Network
+% in the terminal run 
+% clear && roslaunch kortex_gazebo_depth pickplace.launch world:=RoboCup_1.world 
+
 rosshutdown;
-rosinit
+rosinit %('127.0.0.1',11311)
 
 %% Load robot model
 load('exampleHelperKINOVAGen3GripperROSGazebo.mat');
@@ -13,24 +16,28 @@ RoboCupManipulation_setInitialConfig;
 physicsClient = rossvcclient('gazebo/unpause_physics');
 call(physicsClient,'Timeout',3);
 
-
-%% Configure ROS subscriber 
+%% Configure ROS subscribers
 joint_state_sub = rossubscriber('/my_gen3/joint_states');
 ros_action = '/my_gen3/gen3_joint_trajectory_controller/follow_joint_trajectory';
 [trajAct,trajGoalMsg] = rosactionclient(ros_action);
 
-rgbImgSub = rossubscriber('/camera/color/image_raw');     % camera sensor
-rgbDptSub = rossubscriber('/camera/depth/image_raw');     % depth sensor
+ImgSub = rossubscriber('/camera/color/image_raw');     % camera sensor
+DptSub = rossubscriber('/camera/depth/image_raw');     % depth sensor
 
 %% Test image processing
 
-curImage = receive(rgbImgSub);
-img = readImage(rgbImgSub.LatestMessage);
+curImage = receive(ImgSub);
+img = readImage(ImgSub.LatestMessage);
 
-curDepth = receive(rgbDptSub);
-depth = readImage(rgbDptSub.LatestMessage); 
+curDepth = receive(DptSub);
+depth = readImage(DptSub.LatestMessage); 
 %depth=uint8(depth*255);
+%%
+pause(1)
+action='close'
 
+% action='open'
+% gripper(action)
 %% Plot
 close all
 subplot(2,1,1)
